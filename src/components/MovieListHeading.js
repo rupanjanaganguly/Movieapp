f8d8b2a5b33b1f1f3deda08f918b236768bdb899
unknown <rupanjana231@gmail.com>
2023-06-21 16:57:16 +0530

    initial commit

---
+++ src/components/MovieListHeading.js
@@ -0,0 +1,9 @@
+import react from 'react';
+const MovieListHeading = (props) => {
+    return (
+        <div className='col'>
+           <h1>{props.heading}</h1> 
+        </div>
+    );
+};
+export default  MovieListHeading;
